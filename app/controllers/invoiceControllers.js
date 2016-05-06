491f24b115a02825d6cfcc2fb27eba9ffd9ebf3c
Simon Doy <simon@ithinksharepoint.com>
2016-05-06 13:05:27 +0100

    Added data to Invoices in controller

--- app/controllers/invoiceControllers.js
+++ app/controllers/invoiceControllers.js
@@ -10,38 +10,6 @@ invoiceControllersModule.controller('listInvoicesController', ['$scope', functio
     return true;
     };
     
-    function createInvoice(){
-    var invoiceObject={
-        reference: '',
-        companyName: '',
-        invoiceDate: '',
-        contact:'',
-        addressLine1:'',
-        addressLine2:'',
-        addressLine3:'',
-        addressLine4:'',
-        addressCity:'',
-        addressCounty:'',
-        addressPostCode:'',
-        addressCountry:'',
-        agencyName:'',
-        agencyContact: '',
-        invoiceLines: [
-            
-        ],
-        invoiceTotal:0,
-        currencyType:'£',
-        vatRate:0,
-        vatAmount:0,
-        invoiceTotalWithVat:0,
-        status:'',
-        createdBy:'',
-        modifiedBy:'',
-        created:'',
-        modified:'',
-    };
-    return invoiceObject;       
-    };
  
   $scope.load = function(){
       var invoice=createInvoice();
@@ -66,5 +34,67 @@ invoiceControllersModule.controller('listInvoicesController', ['$scope', functio
   
   $scope.load();
   
+  function createInvoice(){
+        var invoiceObject={
+            reference: '',
+            companyName: '',
+            invoiceDate: '',
+            contact:'',
+            addressLine1:'',
+            addressLine2:'',
+            addressLine3:'',
+            addressLine4:'',
+            addressCity:'',
+            addressCounty:'',
+            addressPostCode:'',
+            addressCountry:'',
+            agencyName:'',
+            agencyContact: '',
+            invoiceLines: [
+                
+            ],
+            invoiceTotal:0,
+            currencyType:'£',
+            vatRate:0,
+            vatAmount:0,
+            invoiceTotalWithVat:0,
+            status:'',
+            createdBy:'',
+            modifiedBy:'',
+            created:'',
+            modified:'',
+        };
+        return invoiceObject;       
+    };
   
+  
+}]);
+
+invoiceControllersModule.controller('addInvoiceController', ['$scope', 'dataModelService', '$location', function($scope, dataModelService, $location){
+    $scope.invoice=new dataModelService.Invoice();
+    $scope.addInvoiceLine = function addInvoiceLine() {
+        var newInvoiceLine=new dataModelService.InvoiceLine();
+        $scope.invoice.invoiceLines.push(newInvoiceLine);
+    }
+    
+    $scope.cancel = function cancelStub(){
+        $location.path('/');
+    }
+    
+    $scope.status = "";
+    $scope.error="";
+    
+    $scope.saveInvoice = function saveInvoiceStub(){
+        
+        invoiceDataService.saveNewInvoice($scope.invoiceNo, $scope.invoice).then(function processSuccess(response){
+            $scope.status="Successfully Saved Invoice";
+            $location.path("#");
+            return true;
+        },
+        function processFailure(response){
+            //alert(response.data);
+            $scope.error="Failed to create invoice: " + response.data.message;
+        });
+    };
+    
 }]);
---
+++ app/services/dataModelService.js
@@ -0,0 +1,54 @@
+var dataModelService = angular.module('dataModelService', []);
+
+dataModelService.factory('dataModelService', [dataModelServiceFactory]);
+
+function dataModelServiceFactory()
+{
+    var factory={};
+    factory.Invoice = function invoiceConstructor() {
+        
+        var invoiceObject={
+            reference: '',
+            companyName: '',
+            invoiceDate: '',
+            contact:'',
+            addressLine1:'',
+            addressLine2:'',
+            addressLine3:'',
+            addressLine4:'',
+            addressCity:'',
+            addressCounty:'',
+            addressPostCode:'',
+            addressCountry:'',
+            agencyName:'',
+            agencyContact: '',
+            invoiceLines: [
+                new factory.InvoiceLine() 
+            ],
+            invoiceTotal:0,
+            currencyType:'£',
+            vatRate:0,
+            vatAmount:0,
+            invoiceTotalWithVat:0,
+            status:'',
+            createdBy:'',
+            modifiedBy:'',
+            created:'',
+            modified:''
+        };
+        
+        return invoiceObject;  
+    };
+    
+    factory.InvoiceLine = function invoiceLineConstructor() {
+        var invoiceLine = {
+            description: '', unitQuantity: 0, unitType:'', unitValue:0, lineTotal:0, updateLineTotal: function() {
+                    this.lineTotal=this.unitValue*this.unitQuantity;
+                    return true;
+            }
+        };
+        return invoiceLine;
+    }
+    
+    return factory;
+};
