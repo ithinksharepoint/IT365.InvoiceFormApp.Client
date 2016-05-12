bf2686f694ebd2597a1d4d7fd3ff00ed58d62b25
Simon Doy <simon@ithinksharepoint.com>
2016-05-12 05:53:04 +0100

    updating endpoints

--- app/services/configurationService.js
+++ app/services/configurationService.js
@@ -3,7 +3,7 @@ configurationService.constant('applicationConstants', {
     'clientId':'7c27b6a6-1bb8-43c0-9b92-2eee2264cc71',
     'tenantName':'ithinksharepoint.com',
     'instance':'https://login.microsoftonline.com/',
-    'endPoints': {'https://itspinvoiceformapp.azurewebsites.net/api':'https://apiinvoiceform.ithinksharepoint.com'},
+    'endPoints': {},
 });
 
 
